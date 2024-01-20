1d3fbe59d717f17de38a75d2ed3349c4ac31ca52
DavMInass <davitminasyan95@gmail.com>
2024-01-20 15:27:44 +0400

    add mongodb

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var path = require("path");
 var app = express();
 var bodyParser = require("body-parser")
 
+
+
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'))
@@ -16,7 +18,29 @@ app.post("/addInfo", function(req, res){
     const data1 = req.body.parol
     console.log('this is' + data, data1);
 });
+const mongoose = require('mongoose');
+// Replace the connection string with your MongoDB connection string
+const connectionString = 'mongodb+srv://davitminasyan95:suPerdav1@cluster0.pbftnmd.mongodb.net/sample_mflix';
 
-app.listen(3000, function(){
-   console.log("googleum gri localhost:3000");
-});
+
+// Connect to MongoDB
+mongoose.connect(connectionString, { useUnifiedTopology: true });
+
+// Check the connection
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', async () => {
+console.log('Connected to MongoDB!');
+
+try {
+const allMovies = await mongoose.connection.db.collection('movies').find().toArray(); // .insertMany(newMovies);
+
+console.log('All Movies:', allMovies);
+} catch (error) {
+console.error('Error retrieving movies:', error);
+} finally {
+mongoose.connection.close();
+}
+})
+   
