edab0566d0a194c386ee9c3abd4aa31403800626
Dark07777 <gegham.shagoyan.y@tumo.org>
2024-01-27 15:10:06 +0400

    lesson 3

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
 
-
-
-app.use(bodyParser.urlencoded({ extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get("/", function(req, res){
-   res.sendFile(path.join(__dirname, './public/index.html'))
+const mongoose = require("mongoose");
+
+const connectionString =
+  "mongodb+srv://davitminasyan95:suPerdav1@cluster0.pbftnmd.mongodb.net/sample_mflix";
+
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+
+app.get("/", function (req, res) {
+  res.render("../public/index.ejs" ,{
+      info : mascots,
+      aperjan: 'aper'
+    });
 });
 
-app.post("/addInfo", function(req, res){
-    const data = req.body.login
-    const data1 = req.body.parol
-    console.log('this is' + data, data1);
+app.post("/addInfo", function (req, res) {
+  const data = req.body.login;
+  const data1 = req.body.parol;
+  const data2 = req.res.email;
+  console.log("this is" + data, data1, data2);
+  mongoose.connect(connectionString, { useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "Connection error:"));
+  db.once("open", async () => {
+    console.log("Connected to MongoDB!");
+
+    try {
+      const allMovies = await mongoose.connection.db
+        .collection("users")
+        .insertOne({
+          name: data,
+          email: data1,
+          password: data2,
+        });
+
+      console.log("All Movies:", allMovies);
+    } catch (error) {
+      console.error("Error retrieving movies:", error);
+    } finally {
+      mongoose.connection.close();
+    }
+  });
 });
-const mongoose = require('mongoose');
-// Replace the connection string with your MongoDB connection string
-const connectionString = 'mongodb+srv://davitminasyan95:suPerdav1@cluster0.pbftnmd.mongodb.net/sample_mflix';
 
-
-// Connect to MongoDB
-mongoose.connect(connectionString, { useUnifiedTopology: true });
-
-// Check the connection
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', async () => {
-console.log('Connected to MongoDB!');
-
-try {
-const allMovies = await mongoose.connection.db.collection('movies').find().toArray(); // .insertMany(newMovies);
-
-console.log('All Movies:', allMovies);
-} catch (error) {
-console.error('Error retrieving movies:', error);
-} finally {
-mongoose.connection.close();
-}
-})
-   
+app.listen(3000, function () {
+  console.log("kpanq");
+});
--- public/script.js
+++ public/script.js
@@ -1,15 +1,18 @@
-let login = document.getElementById('myLogin');
-let button = document.getElementById('knopka');
-let parol = document.getElementById('myParol');
-
+let login = document.getElementById("myLogin");
+let button = document.getElementById("knopka");
+let parol = document.getElementById("myParol");
+let email = document.getElementById("myEmail");
 
 async function aper() {
-    await fetch("http://localhost:3000/addInfo",{
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify({login: login.value , parol: parol.value})
-        })
-        
+  await fetch("http://localhost:3000/addInfo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      login: login.value,
+      parol: parol.value,
+      email: email.value,
+    }),
+  });
 }
